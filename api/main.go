d8cd66dd334203bfff1ee23782ca743519ccccb6
Marco Simon Zillgen <marcosimonzillgen@gmail.com>
2024-11-12 17:01:47 +0100

    refactor: restructure Notion data handling and API endpoints

--- api/main.go
+++ api/main.go
@@ -5,15 +5,20 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/MarcoZillgen/uni_plan/notion"
 	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// load data
 	godotenv.Load()
 	PORT := os.Getenv("API_PORT")
+
+	// api router
 	r := mux.NewRouter()
-	r.HandleFunc("/api/notion/data", getNotionData).Methods("GET")
+	r.HandleFunc("/api/notion/data", notion.GetDefaultData).Methods("GET")
+	r.HandleFunc("/api/notion/data", notion.GetData).Methods("GET").Queries("notionKey", "{notionKey}", "dbID", "{dbID}")
 	log.Println("Server is running on port " + PORT)
 	log.Fatal(http.ListenAndServe(":"+PORT, r))
 }
--- notion/data.go
+++ notion/data.go
@@ -1,4 +1,4 @@
-package main
+package notion
 
 import (
 	"context"
@@ -7,17 +7,38 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/gorilla/mux"
 	"github.com/joho/godotenv"
 	"github.com/jomei/notionapi"
 )
 
-func getNotionData(w http.ResponseWriter, r *http.Request) {
+func GetDefaultData(w http.ResponseWriter, r *http.Request) {
 	godotenv.Load()
-	NOTION_KEY := os.Getenv("NOTION_KEY")
-	DB_ID := os.Getenv("NOTION_DB_ID")
+	notionKey := os.Getenv("NOTION_KEY")
+	dbId := os.Getenv("NOTION_DB_ID")
 
-	client := notionapi.NewClient(notionapi.Token(NOTION_KEY))
-	db, err := client.Database.Query(context.Background(), notionapi.DatabaseID(DB_ID), nil)
+	dataHelper(w, notionKey, dbId)
+}
+
+func GetData(w http.ResponseWriter, r *http.Request) {
+	notionKey := mux.Vars(r)["notionKey"]
+	dbId := mux.Vars(r)["dbId"]
+	dataHelper(w, notionKey, dbId)
+}
+
+type EventData struct {
+	Title     string
+	StartTime float64
+	EndTime   float64
+	Color     string
+	EventType string
+	Day       string // monday, tuesday, etc.
+	Place     string
+}
+
+func dataHelper(w http.ResponseWriter, notionKey string, dbId string) {
+	client := notionapi.NewClient(notionapi.Token(notionKey))
+	db, err := client.Database.Query(context.Background(), notionapi.DatabaseID(dbId), nil)
 	if err != nil {
 		panic(err)
 	}
@@ -33,9 +54,13 @@ func getNotionData(w http.ResponseWriter, r *http.Request) {
 		dayProp, ok3 := page.Properties["Day"].(*notionapi.SelectProperty)
 		colorProp, ok4 := page.Properties["Color"].(*notionapi.SelectProperty)
 		typeProp, ok5 := page.Properties["Type"].(*notionapi.SelectProperty)
+		placeProp, ok6 := page.Properties["Place"].(*notionapi.RichTextProperty)
 
-		if !ok || !ok1 || !ok2 || !ok3 || !ok4 || !ok5 {
+		if !ok || !ok1 || !ok2 || !ok3 || !ok4 || !ok5 || !ok6 {
 			fmt.Println("Missing property/properties in page: " + page.ID)
+			fmt.Println(ok, ok1, ok2, ok3, ok4, ok5, ok6)
+			fmt.Println("Skipping this page")
+			fmt.Println()
 			continue
 		}
 
@@ -46,22 +71,17 @@ func getNotionData(w http.ResponseWriter, r *http.Request) {
 			Day:       dayProp.Select.Name,
 			Color:     colorProp.Select.Name,
 			EventType: typeProp.Select.Name,
+			Place:     placeProp.RichText[0].PlainText,
 		}
 
 		eventDatas = append(eventDatas, event)
 	}
 
+	fmt.Println("Data fetched successfully")
+	fmt.Println(eventDatas)
+
 	w.Header().Set("Content-Type", "application/json")
 	if err := json.NewEncoder(w).Encode(eventDatas); err != nil {
 		http.Error(w, fmt.Sprintf("Failed to encode response: %v", err), http.StatusInternalServerError)
 	}
-}
-
-type EventData struct {
-	Title     string
-	StartTime float64
-	EndTime   float64
-	Color     string
-	EventType string
-	Day       string // monday, tuesday, etc.
 }
